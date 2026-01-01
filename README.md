d42737e1f836a65db49a59b2f2c293fd66ab5fa4
egmg726 <email.egail@gmail.com>
2026-01-02 09:51:14 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Objective
+
+Perform a simple exploratory analysis of RNA-seq expression data and associated metadata.
+
+# Tasks
+
+1. Fork this repository
+2. Complete the code in analysis.R
+3. Save your plot to the results/ directory
+4. Commit the changes to the with a meaningful
+5. Complete the questions in the application form
+
---
+++ analysis.r
@@ -0,0 +1,26 @@
+# Melbourne Bioinformatics Training Program
+
+
+
+# Load libraries -------------------
+# You may use base R or tidyverse for this exercise
+
+
+# Load data here ----------------------
+
+
+# Inspect the data -------------------------
+
+# What are the dimensions of each dataset? (How many rows/columns in each?)
+
+## Expression data
+
+
+## Metadata
+
+
+# Plot the data --------------------------
+## Plot the expression by cell type
+
+
+## Save the plot
